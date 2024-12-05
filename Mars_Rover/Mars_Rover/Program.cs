910533612a4261b5732ee48dfa9f441d98bbff39
termgiles <Thomas.giles2222@gmail.com>
2024-12-05 11:31:08 +0000

    feat: add rover class

---
+++ Mars_Rover/Mars_Rover/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mars_Rover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Mars_Rover/Mars_Rover/Rover.cs
@@ -0,0 +1,16 @@
+﻿namespace Mars_Rover
+{
+    public class Rover : //IGridElement?
+    {
+        public string Name { get; private set; }
+        public Compass Orientation { get; set; }
+        public List<Compass> History { get; private set; }
+
+        public Rover(string name, Compass orientation)
+        {
+            this.Name = name;
+            this.Orientation = orientation;
+        }
+
+    }
+}
