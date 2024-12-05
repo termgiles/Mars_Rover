83ad4c117d9567c7639b7d5e1c0fca1001b29dbb
termgiles <Thomas.giles2222@gmail.com>
2024-12-05 14:55:51 +0000

    feat: add input parser for Rover Instructions

---
+++ Mars_Rover/Mars_Rover/InputParser.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace Mars_Rover
+{
+    public class InputParser 
+    {
+        public static List<Instruction> ParseInstruction(string instructions)
+        {
+            List<Instruction> output = new List<Instruction>();
+            char[] roverInstructions = new char[3] { 'L', 'R', 'M' };
+            try
+            {
+                foreach (char c in instructions.ToUpper())
+                {
+                    if (roverInstructions.Contains(c))
+                    {
+                        switch(c)
+                        {
+                            case 'L':
+                                output.Add(Instruction.L);
+                                break;
+                            case 'R':
+                                output.Add(Instruction.R);
+                                break;
+                            case 'M':
+                                output.Add(Instruction.M);
+                                break;
+                        };
+                    }
+                }
+                if (output.Count > 0)
+                {
+                    return output;
+                }
+                else
+                {
+                    throw new Exception("no recognised rover inputs");
+                }
+            }
+            catch
+            {
+                throw new Exception("no recognised rover inputs");
+            }
+        }
+    }
+}
--- Mars_Rover/Mars_Rover_Tests/Testing/Rover_tests.cs
+++ Mars_Rover/Mars_Rover_Tests/Testing/Rover_tests.cs
@@ -1,16 +1,54 @@
+using System.Reflection.Metadata.Ecma335;
+using Mars_Rover;
+using FluentAssertions;
+
 namespace Mars_Rover_Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void InstructionParse_ShouldParseNeatInputs()
         {
+            string input = "LMMMRMMMLR";
+            List<Instruction> expectdOutput = new List<Instruction> { Instruction.L, Instruction.M, Instruction.M, Instruction.M, Instruction.R, Instruction.M, Instruction.M, Instruction.M, Instruction.L, Instruction.R };
+
+
+            InputParser.ParseInstruction(input).Should().BeEquivalentTo(expectdOutput);
         }
 
         [Test]
-        public void Test1()
+        public void InstructionParse_ShouldParseMessyInputs()
         {
-            Assert.Pass();
+            string input = "wordIJ3-';mMLMRoo9 dfdsM";
+            List<Instruction> expectdOutput = new List<Instruction> {Instruction.R,Instruction.M, Instruction.M, Instruction.L, Instruction.M, Instruction.R, Instruction.M};
+
+
+            InputParser.ParseInstruction(input).Should().BeEquivalentTo(expectdOutput);
+        }
+
+        [Test]
+        public void InstructionParse_ShouldThrowNoInput()
+        {
+            string input = "noinputsinthis";
+
+            try
+            {
+                InputParser.ParseInstruction(input);
+                Assert.Fail();
+            }
+            catch(Exception ex)
+            {
+                if (ex.Message == "no recognised rover inputs")
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                   Assert.Fail();
+                }
+
+            }
         }
     }
 }
