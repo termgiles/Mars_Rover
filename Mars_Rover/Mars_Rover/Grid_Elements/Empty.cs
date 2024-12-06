fcc520b4e5c0b0292e0e9871e38f0fe7c2b6d4c9
termgiles <Thomas.giles2222@gmail.com>
2024-12-06 12:13:35 +0000

    feat: add Empty grid element

---
+++ Mars_Rover/Mars_Rover/Grid_Elements/Empty.cs
@@ -0,0 +1,16 @@
+﻿namespace Mars_Rover
+{
+    public class Empty : IGridElement 
+    {
+        public static int Count = 0;
+        public char Symbol { get; } = ' ';
+        public bool IsSolid { get; } = false;
+        public int ID { get; } = 0;
+        public Empty()
+        {
+            this.ID = Count + 1;
+            Count++;
+        }
+    }
+
+}
