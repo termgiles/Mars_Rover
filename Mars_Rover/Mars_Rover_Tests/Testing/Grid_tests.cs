b6fc576f3f508f94cd78b33aeace3d07609a3a31
termgiles <Thomas.giles2222@gmail.com>
2024-12-06 12:47:55 +0000

    test: add to test pattern for grid logic

---
+++ Mars_Rover/Mars_Rover_Tests/Testing/Grid_tests.cs
@@ -0,0 +1,27 @@
+﻿namespace Mars_Rover_Tests
+{
+    public class Grid_tests 
+    {
+        [Test]
+        public void Test_Rover_CantMoveOffGrid()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_Rover_CanMoveToEmpty()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_Rover_CantMoveThroughSolid()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_Rover_CanMoveToUnSolid()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
