db0097237e9ec5cadcc3f5ad7b7de76e27ba25f1
termgiles <thomas.giles2222@gmail.com>
2025-03-11 13:39:20 +0000

    Stability improvements: main menu now catches incorectly formatted meny options

--- Mars_Rover/Mars_Rover/UserInterfaces/MainMenu.cs
+++ Mars_Rover/Mars_Rover/UserInterfaces/MainMenu.cs
@@ -8,24 +8,34 @@
             Transition nextState;
             bool validResponse = false;
             string response = null;
-            response = Console.ReadLine();
-            if(Int32.Parse(response) == 1)
+            while (!validResponse)
             {
-                validResponse = true;
-                Console.Clear();
-                return Transition.DISPLAY;
-            }
-            if (Int32.Parse(response) == 2)
-            {
-                validResponse = true;
-                Console.Clear();
-                return Transition.SETTINGS;
-            }
-            if (Int32.Parse(response) == 3)
-            {
-                validResponse = true;
-                Console.Clear();
-                return Transition.QUIT;
+                try
+                {
+                    response = Console.ReadLine();
+                    if(Int32.Parse(response) == 1)
+                    {
+                        validResponse = true;
+                        Console.Clear();
+                        return Transition.DISPLAY;
+                    }
+                    if (Int32.Parse(response) == 2)
+                    {
+                        validResponse = true;
+                        Console.Clear();
+                        return Transition.SETTINGS;
+                    }
+                    if (Int32.Parse(response) == 3)
+                    {
+                        validResponse = true;
+                        Console.Clear();
+                        return Transition.QUIT;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "\nEnter another response");
+                }
             }
    
             return Transition.MAIN_MENU;
