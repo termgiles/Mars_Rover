cbdca1e5d17770d651ca222eebb8201a71804b7d
termgiles <thomas.giles2222@gmail.com>
2025-03-14 04:48:50 +0000

    feat: settings interface

--- Mars_Rover/Mars_Rover/UserInterfaces/Settings.cs
+++ Mars_Rover/Mars_Rover/UserInterfaces/Settings.cs
@@ -4,10 +4,39 @@
     {
         public bool DefaultSettings { get; private set; } = true;
 
+        public bool AnalougeMode { get; private set; } = true;
+
         public override Transition Run() 
         {
-            Console.WriteLine("settings:");
-            return Transition.LOADING_SCREEN;       //add
+            Transition nextState = Transition.LOADING_SCREEN;
+
+            bool stillParsing = true;
+
+            while (stillParsing)
+            {
+                Console.Clear();
+                Console.WriteLine("settings: \n");
+                Console.WriteLine("Type 'D' to toggle default settings, 'A' to toggle analogue mode");
+                Console.WriteLine("Type 2 for main menu, 3 to quit:");
+
+                Console.WriteLine("Default settings: " + DefaultSettings.ToString());
+                Console.WriteLine("Analogue mode: " + AnalougeMode.ToString());
+                Console.Write("\n");
+                string response = Console.ReadLine();
+                if (response == "3")
+                {
+                    nextState = Transition.QUIT;
+                    stillParsing = false;
+                }
+                if (response == "2")
+                {
+                    stillParsing = false;
+                }
+                if (response.ToUpper().Trim() == "A") AnalougeMode = !AnalougeMode;
+                if( response.ToUpper().Trim() == "D") DefaultSettings = !DefaultSettings;
+            }
+
+            return nextState;       
         }
     }
 }
